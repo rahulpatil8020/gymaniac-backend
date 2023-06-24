c4a04561e804424bfa663a6376c370a30a89d92b
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-06-24 14:12:28 -0700

    Added auth controllers for auth routes and used the middleware for login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,60 +3,81 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
-const login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (user) {
-      const isPasswordCorrect = await bcrypt.compare(password, user.password);
-      if (isPasswordCorrect) {
-        const token = jwt.sign(
-          { id: user._id, email: user.email },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({ token, user });
-      } else {
-        return res.status(401).json({ message: "Invalid Credentials" });
-      }
-    } else return res.status(404).json({ message: "User does not exist" });
-  } catch (error) {
-    res.status(500).json({ message: "Something went Wrong" });
-  }
-};
+// const login = async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     const user = await User.findOne({ email });
+//     if (user) {
+//       const isPasswordCorrect = await bcrypt.compare(password, user.password);
+//       if (isPasswordCorrect) {
+//         const token = jwt.sign(
+//           { id: user._id, email: user.email },
+//           process.env.JWT_SECRET,
+//           { expiresIn: "1h" }
+//         );
+//         res.status(200).json({ token, user });
+//       } else {
+//         return res.status(401).json({ message: "Invalid Credentials" });
+//       }
+//     } else return res.status(404).json({ message: "User does not exist" });
+//   } catch (error) {
+//     res.status(500).json({ message: "Something went Wrong" });
+//   }
+// };
 
-const signup = async (req, res) => {
-  const user = req.body;
-  try {
-    const existingUser = await User.findOne({ email: user.email })
-      .lean()
-      .exec();
-    if (existingUser) {
-      return res.status(409).json({ message: "User already exists" });
-    }
-    if (user.password !== user.confirmPassword)
-      return res.status(409).json({ message: "Passwords don't match" });
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
-    const hashedPassword = await bcrypt.hash(user.password, salt);
+// const signup = async (req, res) => {
+//   const user = req.body;
+//   try {
+//     const existingUser = await User.findOne({ email: user.email })
+//       .lean()
+//       .exec();
+//     if (existingUser) {
+//       return res.status(409).json({ message: "User already exists" });
+//     }
+//     if (user.password !== user.confirmPassword)
+//       return res.status(409).json({ message: "Passwords don't match" });
+//     const salt = await bcrypt.genSalt(Number(process.env.SALT));
+//     const hashedPassword = await bcrypt.hash(user.password, salt);
 
-    const newUser = new User({
-      email: user.email,
-      password: hashedPassword,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      role: "trainee",
-      level: "beginner",
-    });
-    await newUser.save();
-    const token = jwt.sign(
-      { id: newUser._id, email: newUser.email },
-      process.env.JWT_SECRET,
-      { expiresIn: "1hr" }
-    );
-    res.status(200).json({ token, user: newUser });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-  }
-};
+//     const newUser = new User({
+//       email: user.email,
+//       password: hashedPassword,
+//       firstName: user.firstName,
+//       lastName: user.lastName,
+//       role: "trainee",
+//       level: "beginner",
+//     });
+//     await newUser.save();
+//     const token = jwt.sign(
+//       { id: newUser._id, email: newUser.email },
+//       process.env.JWT_SECRET,
+//       { expiresIn: "1hr" }
+//     );
+//     res.status(200).json({ token, user: newUser });
+//   } catch (error) {
+//     res.status(500).json({ message: "Something went wrong" });
+//   }
+// };
 
-module.exports = { login, signup };
+//@desc Login
+//@route /auth/login
+//@access public
+
+const login = async (req, res) => {};
+
+//desc Signup
+//@route /auth/signup
+//@access public
+
+const signup = async (req, res) => {};
+
+//@desc Refresh
+//@route /auth/refresh
+//@access Public - Access token refreshed after expiration
+const refresh = async (req, res) => {};
+
+//@desc Logout
+//@route POST /auth/logout
+//@access Public - Clear the cookies
+const logout = async (req, res) => {};
+module.exports = { login, signup, refresh, logout };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { login, signup } = require("../controllers/authController");
+const {
+  login,
+  signup,
+  refresh,
+  logout,
+} = require("../controllers/authController");
+const loginLimiter = require("../middleware/loginLimiter");
 
 const router = express.Router();
-router.post("/login", login);
-router.post("/signup", signup);
+
+router.route("/login").post(loginLimiter, login);
+router.route("/signup").post(signup);
+router.route("/refresh").get(refresh);
+router.route("/logout").post(logout);
 
 module.exports = router;
