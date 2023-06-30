648e0fad0175ff3486d2f6786ff2bddc1d98521d
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-06-30 11:18:55 -0700

    Signup controller fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,62 +3,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
-// const login = async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     const user = await User.findOne({ email });
-//     if (user) {
-//       const isPasswordCorrect = await bcrypt.compare(password, user.password);
-//       if (isPasswordCorrect) {
-//         const token = jwt.sign(
-//           { id: user._id, email: user.email },
-//           process.env.JWT_SECRET,
-//           { expiresIn: "1h" }
-//         );
-//         res.status(200).json({ token, user });
-//       } else {
-//         return res.status(401).json({ message: "Invalid Credentials" });
-//       }
-//     } else return res.status(404).json({ message: "User does not exist" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Something went Wrong" });
-//   }
-// };
-
-// const signup = async (req, res) => {
-//   const user = req.body;
-//   try {
-//     const existingUser = await User.findOne({ email: user.email })
-//       .lean()
-//       .exec();
-//     if (existingUser) {
-//       return res.status(409).json({ message: "User already exists" });
-//     }
-//     if (user.password !== user.confirmPassword)
-//       return res.status(409).json({ message: "Passwords don't match" });
-// const salt = await bcrypt.genSalt(Number(process.env.SALT));
-// const hashedPassword = await bcrypt.hash(user.password, salt);
-
-// const newUser = new User({
-//   email: user.email,
-//   password: hashedPassword,
-//   firstName: user.firstName,
-//   lastName: user.lastName,
-//   role: "trainee",
-//   level: "beginner",
-// });
-// await newUser.save();
-//     const token = jwt.sign(
-//       { id: newUser._id, email: newUser.email },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "1hr" }
-//     );
-//     res.status(200).json({ token, user: newUser });
-//   } catch (error) {
-//     res.status(500).json({ message: "Something went wrong" });
-//   }
-// };
-
 // @desc Login
 // @route /auth/login
 // @access public
@@ -109,17 +53,15 @@ const login = async (req, res) => {
 
 const signup = async (req, res) => {
   const user = req.body;
-
   try {
-    const duplicate = await User.findOne({ email })
+    const duplicate = await User.findOne({ username: user.username })
       .collation({ locale: "en", strength: 2 })
       .lean()
       .exec();
-
     if (duplicate)
       return res
         .status(409)
-        .json({ message: "An account already exists with this email" });
+        .json({ message: "An account already exists with this username" });
 
     const salt = await bcrypt.genSalt(Number(process.env.SALT));
     const hashedPassword = await bcrypt.hash(user.password, salt);
@@ -128,15 +70,16 @@ const signup = async (req, res) => {
       password: hashedPassword,
       firstName: user.firstName,
       lastName: user.lastName,
+      username: user.username,
       role: "trainee",
       level: "beginner",
     });
     await newUser.save();
-    if (user) res.status(201).json({ message: "Accoutn created successfully" });
+    if (user) res.status(201).json({ message: "Account created successfully" });
     else
       res
         .status(400)
-        .json({ message: "Error  creating account. Please try again later" });
+        .json({ message: "Error creating account. Please try again later" });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
